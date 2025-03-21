513f85671d475ee4fe8b51df1530948783852392
Ekasestao <ekaitzcampo2002@gmail.com>
2025-03-21 14:21:44 +0100

    Changing modifyTask

--- M.A.E.K_Project/src/view/WindowModifyTask.java
+++ M.A.E.K_Project/src/view/WindowModifyTask.java
@@ -2,10 +2,12 @@ package view;
 
 import java.awt.BorderLayout;
 
+import javax.swing.ButtonGroup;
 import javax.swing.JButton;
 import javax.swing.JDialog;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
+import javax.swing.JRadioButton;
 import javax.swing.border.EmptyBorder;
 
 import controller.Controller;
@@ -14,22 +16,26 @@ import model.Client;
 import javax.swing.JLabel;
 import javax.swing.SwingConstants;
 import java.awt.Font;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
 import javax.swing.JComboBox;
-import javax.swing.JCheckBox;
 import java.awt.Color;
 import javax.swing.JTextField;
 
-public class WindowModifyTask extends JDialog {
+public class WindowModifyTask extends JDialog implements ActionListener {
 	private static final long serialVersionUID = 1L;
 	
 	private final JPanel contentPanel = new JPanel();
-	private JTextField textFieldID;
 	private JTextField textFieldName;
 	private JTextField textFieldDescription;
 	private JTextField textFieldDueDate;
 	private JTextField textFieldCategory;
 	private JButton btnExit;
 	private Controller cont;
+	private JRadioButton rdbtnPending;
+	private JRadioButton rdbtnCompleted;
+	private ButtonGroup btnGroupPC;
 
 	public WindowModifyTask(JFrame parent, Client client, Controller cont) {
 		super(parent, true);
@@ -53,37 +59,24 @@ public class WindowModifyTask extends JDialog {
 		comboBox.setBounds(19, 104, 490, 29);
 		contentPanel.add(comboBox);
 		
-		JCheckBox chckbxPending = new JCheckBox("Pending");
-		chckbxPending.setBorder(null);
-		chckbxPending.setForeground(new Color(33, 37, 41));
-		chckbxPending.setFont(new Font("Source Code Pro", Font.PLAIN, 24));
-		chckbxPending.setBounds(18, 148, 157, 34);
-		contentPanel.add(chckbxPending);
-		
-		JCheckBox chckbxCompleted = new JCheckBox("Completed");
-		chckbxCompleted.setBorder(null);
-		chckbxCompleted.setFont(new Font("Source Code Pro", Font.PLAIN, 24));
-		chckbxCompleted.setBounds(18, 194, 183, 34);
-		contentPanel.add(chckbxCompleted);
-		
-		JLabel lblID = new JLabel("ID");
-		lblID.setHorizontalAlignment(SwingConstants.LEFT);
-		lblID.setFont(new Font("Source Code Pro", Font.PLAIN, 24));
-		lblID.setBounds(269, 152, 184, 34);
-		contentPanel.add(lblID);
-		
-		textFieldID = new JTextField();
-		textFieldID.setBackground(new Color(173, 181, 189));
-		textFieldID.setBorder(null);
-		textFieldID.setFont(new Font("Source Code Pro", Font.PLAIN, 24));
-		textFieldID.setBounds(269, 182, 240, 31);
-		contentPanel.add(textFieldID);
-		textFieldID.setColumns(10);
+		rdbtnPending = new JRadioButton("Pending");
+		rdbtnPending.setFont(new Font("Source Code Pro", Font.PLAIN, 24));
+		rdbtnPending.setBounds(19, 164, 130, 34);
+		contentPanel.add(rdbtnPending);
+		rdbtnPending.setSelected(true);
+
+		rdbtnCompleted = new JRadioButton("Completed");
+		rdbtnCompleted.setFont(new Font("Source Code Pro", Font.PLAIN, 24));
+		rdbtnCompleted.setBounds(19, 201, 157, 34);
+		contentPanel.add(rdbtnCompleted);
+		btnGroupPC = new ButtonGroup();
+		btnGroupPC.add(rdbtnPending);
+		btnGroupPC.add(rdbtnCompleted);
 		
 		JLabel lblName = new JLabel("Name");
 		lblName.setHorizontalAlignment(SwingConstants.LEFT);
 		lblName.setFont(new Font("Source Code Pro", Font.PLAIN, 24));
-		lblName.setBounds(19, 239, 184, 34);
+		lblName.setBounds(296, 164, 184, 34);
 		contentPanel.add(lblName);
 		
 		textFieldName = new JTextField();
@@ -91,13 +84,13 @@ public class WindowModifyTask extends JDialog {
 		textFieldName.setFont(new Font("Source Code Pro", Font.PLAIN, 24));
 		textFieldName.setBorder(null);
 		textFieldName.setColumns(10);
-		textFieldName.setBounds(18, 269, 240, 31);
+		textFieldName.setBounds(295, 194, 214, 31);
 		contentPanel.add(textFieldName);
 		
 		JLabel lblDescription = new JLabel("Description");
 		lblDescription.setHorizontalAlignment(SwingConstants.LEFT);
 		lblDescription.setFont(new Font("Source Code Pro", Font.PLAIN, 24));
-		lblDescription.setBounds(269, 239, 184, 34);
+		lblDescription.setBounds(19, 242, 184, 34);
 		contentPanel.add(lblDescription);
 		
 		textFieldDescription = new JTextField();
@@ -105,7 +98,7 @@ public class WindowModifyTask extends JDialog {
 		textFieldDescription.setFont(new Font("Source Code Pro", Font.PLAIN, 24));
 		textFieldDescription.setBorder(null);
 		textFieldDescription.setColumns(10);
-		textFieldDescription.setBounds(269, 269, 240, 31);
+		textFieldDescription.setBounds(19, 272, 214, 31);
 		contentPanel.add(textFieldDescription);
 		
 		textFieldDueDate = new JTextField();
@@ -113,13 +106,13 @@ public class WindowModifyTask extends JDialog {
 		textFieldDueDate.setFont(new Font("Source Code Pro", Font.PLAIN, 24));
 		textFieldDueDate.setBorder(null);
 		textFieldDueDate.setColumns(10);
-		textFieldDueDate.setBounds(18, 354, 240, 31);
+		textFieldDueDate.setBounds(19, 355, 288, 31);
 		contentPanel.add(textFieldDueDate);
 		
-		JLabel lblDueDate = new JLabel("Due Date");
+		JLabel lblDueDate = new JLabel("Due date (yyyy-MM-dd)");
 		lblDueDate.setHorizontalAlignment(SwingConstants.LEFT);
 		lblDueDate.setFont(new Font("Source Code Pro", Font.PLAIN, 24));
-		lblDueDate.setBounds(18, 324, 184, 34);
+		lblDueDate.setBounds(19, 318, 343, 34);
 		contentPanel.add(lblDueDate);
 		
 		textFieldCategory = new JTextField();
@@ -127,13 +120,13 @@ public class WindowModifyTask extends JDialog {
 		textFieldCategory.setFont(new Font("Source Code Pro", Font.PLAIN, 24));
 		textFieldCategory.setBorder(null);
 		textFieldCategory.setColumns(10);
-		textFieldCategory.setBounds(269, 354, 240, 31);
+		textFieldCategory.setBounds(296, 272, 214, 31);
 		contentPanel.add(textFieldCategory);
 		
 		JLabel lblCategory = new JLabel("Category");
 		lblCategory.setHorizontalAlignment(SwingConstants.LEFT);
 		lblCategory.setFont(new Font("Source Code Pro", Font.PLAIN, 24));
-		lblCategory.setBounds(269, 324, 184, 34);
+		lblCategory.setBounds(296, 238, 184, 34);
 		contentPanel.add(lblCategory);
 		
 		btnExit = new JButton("Exit");
@@ -142,5 +135,11 @@ public class WindowModifyTask extends JDialog {
 		btnExit.setBackground(new Color(33, 37, 41));
 		btnExit.setBounds(414, 406, 112, 27);
 		contentPanel.add(btnExit);
+	}
+
+	@Override
+	public void actionPerformed(ActionEvent e) {
+		
+		
 	}
 }
